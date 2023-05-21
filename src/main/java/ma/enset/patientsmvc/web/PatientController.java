fb14c173ebea7dd43c46cdc26afe026a8cca82fd
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-05-21 18:05:58 +0100

    version amélioré avec edit form et navbar

--- src/main/java/ma/enset/patientsmvc/web/PatientController.java
+++ src/main/java/ma/enset/patientsmvc/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.patientsmvc.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.patientsmvc.entities.Patient;
 import ma.enset.patientsmvc.repositories.PatientRepository;
@@ -7,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -49,8 +52,30 @@ public class PatientController {
 
     @GetMapping("/patients")
     @ResponseBody
-    public List<Patient> lisPatients(){
-        return patientRepository.findAll();
+    public List<Patient> lisPatients(){return patientRepository.findAll();}
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
     }
 
+    @PostMapping(path = "/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "")String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id, String keyword , int page){
+        Patient patient = patientRepository.findById(id).orElse( null);
+        if(patient==null) throw  new RuntimeException("Patient introuvable");
+        model.addAttribute("patient", patient);
+        model.addAttribute("page", page);
+        model.addAttribute("keyword", keyword);
+        return "editPatient";
+    }
 }
